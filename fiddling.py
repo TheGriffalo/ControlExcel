2aecac48a2d00e26dd45e66db72a6eeffb9a529d
TheGriffalo <paul@griffin.me.uk>
2020-04-30 21:00:47 +0100

    Lesson 9

--- fiddling.py
+++ fiddling.py
@@ -1,5 +1,25 @@
 import openpyxl
 
-wb = openpyxl.Workbook()
-wb.save("My first workbook.xlsx")
+from openpyxl import Workbook
 
+#Create the workbook object
+wb = Workbook()
+
+#Create a new sheet
+ws = wb.create_sheet("A Sheet", 0) #Insert at first position
+
+#Change the name of the sheet
+ws.title = "Hello World"
+
+#Create another sheet
+ws2 = wb.create_sheet("Sheet nr 2")
+
+
+
+wb.remove(wb["Sheet"])
+
+
+wb.save("Lesson_9.xlsx")
+
+for sheet in wb:
+    print(sheet.title)
